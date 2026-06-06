02af796bfa9530f9a58299d91aa37face7ad3367
nathnael <134805961+nchanyal@users.noreply.github.com>
2026-06-06 15:48:29 -0400

    add functionality to display notifications

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,17 @@
+import { Notifications } from "@/components/Notifications";
+import { SuggestedUsers } from "@/components/SuggestedUsers";
+
+export default function NotificationsPage() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        <div className="space-y-4">
+          <Notifications />
+        </div>
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <SuggestedUsers />
+      </div>
+    </div>
+  );
+}
