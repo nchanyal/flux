354a8b14d64011e4524845104b6387af756e27c4
nathnael <134805961+nchanyal@users.noreply.github.com>
2026-07-10 16:54:17 -0400

    protect notifications route

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -2,8 +2,12 @@ export const dynamic = "force-dynamic";
 
 import { Notifications } from "@/components/Notifications";
 import { SuggestedUsers } from "@/components/SuggestedUsers";
+import { auth } from "@clerk/nextjs/server";
 
-export default function NotificationsPage() {
+export default async function NotificationsPage() {
+  // Protect the route if the user is not signed in
+  await auth.protect();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
